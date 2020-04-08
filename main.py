f95142eb70874070018af74d8620072b1acaae60
LongNC <long.nguyen2@gigasource.io>
2020-04-08 18:14:06 +0700

    detect spoofing

--- main.py
+++ main.py
@@ -59,21 +59,20 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    # config_args = parse_args()
-    # config_args.img_height, config_args.img_width, config_args.num_channels = (224, 224, 3)
-    # _, config_args.summary_dir, config_args.checkpoint_dir = create_experiment_dirs(config_args.experiment_dir)
-    # config = tf.ConfigProto(allow_soft_placement=True)
-    # config.gpu_options.allow_growth = True
-    # faces = FaceCropper().generate('maxresdefault.jpg')
-    # with tf.Session(config=config) as sess:
-    #     config_args.batch_size = len(faces)
-    #     model = MobileNet(config_args)
-    #     sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
-    #     saver = tf.train.Saver(max_to_keep=config_args.max_to_keep,
-    #                                 keep_checkpoint_every_n_hours=10,
-    #                                 save_relative_paths=True)
-    #     saver.restore(sess, tf.train.latest_checkpoint(config_args.checkpoint_dir))
-    #     sess.run(model.y_out_argmax, feed_dict={model.X: faces, model.is_training: False})
+    config_args = parse_args()
+    config_args.img_height, config_args.img_width, config_args.num_channels = (224, 224, 3)
+    _, config_args.summary_dir, config_args.checkpoint_dir = create_experiment_dirs(config_args.experiment_dir)
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    faces = FaceCropper().generate('maxresdefault.jpg')
+    with tf.Session(config=config) as sess:
+        config_args.batch_size = len(faces)
+        model = MobileNet(config_args)
+        sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
+        saver = tf.train.Saver(max_to_keep=config_args.max_to_keep,
+                                    keep_checkpoint_every_n_hours=10,
+                                    save_relative_paths=True)
+        saver.restore(sess, tf.train.latest_checkpoint(config_args.checkpoint_dir))
 
         # show camera
         face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -82,8 +81,29 @@ if __name__ == '__main__':
             _, img = cap.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+            croppedFaces = []
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                r = max(w, h) / 2
+                centerx = x + w / 2
+                centery = y + h / 2
+                nx = int(centerx - r)
+                ny = int(centery - r)
+                nr = int(r * 2)
+
+                faceimg = img[ny:ny+nr, nx:nx+nr]
+                lastimg = cv2.resize(faceimg, (224, 224))
+                croppedFaces.append(lastimg)
+        
+            normalizedFaces = np.array(croppedFaces).reshape((len(croppedFaces), 224, 224, 3))
+            results = sess.run(model.y_out_argmax, feed_dict={model.X: normalizedFaces, model.is_training: False})
+            i = 0
+            for (x, y, w, h) in faces:
+                if (results[i] == 0):
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+                    cv2.putText(img, 'Fake', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 0), 2)
+                if (results[i] == 1):
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                    cv2.putText(img, 'Fake', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 0), 2)
             cv2.imshow('img', img)
             k = cv2.waitKey(30) & 0xff
             if k==27:
